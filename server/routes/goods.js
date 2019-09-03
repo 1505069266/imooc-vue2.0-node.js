218ba13ce29df9d21014e0c43f256083f4e1723d
zhuxiaole <1505069266@qq.com>
2019-09-03 17:06:48 +0800

    数据库OK

--- server/routes/goods.js
+++ server/routes/goods.js
@@ -13,24 +13,37 @@ Goods.connect((err)=>{
     }
 })
 
-let goodsList = null
-Goods.query('select name from users;',(err,res,fields)=>{
-    if(err){
-        goodsList = err
-        return
-    }else{
-        goodsList =res
-    }
-})
+// let goodsList = null
+// Goods.query('select id from users;',(err,res,fields)=>{
+//     if(err){
+//         goodsList = err
+//         return
+//     }else{
+//         goodsList =res
+//     }
+// })
 
 router.get("/",(req,res,next)=>{
-
-    res.json({
-        status:"0",
-        msg:'',
-        result:{
-            name:goodsList
+    let page = req.param("page") //当前页数
+    let pageSize = req.param("pageSize") //每页多少条数据
+    let sort = req.param('sort')
+    let name = req.param('name')
+    let skip = (page-1) * pageSize
+    let q = `SELECT * FROM users where name=${name} LIMIT  ${skip},${pageSize} `
+    Goods.query(q,(err,ress,fields)=>{
+        if(err){
+            res.status(503).json({
+                error:'查询错误'
+            })
+            return
         }
+        res.json({
+            status:'0',
+            msg:'',
+            result:{
+                peoples:ress
+            }
+        })
     })
 })
 
